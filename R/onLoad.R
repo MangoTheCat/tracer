2b391adeb332fe834dd34a45d771cc986a7b4791
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-16 17:25:10 +0000

    Set (dummy) dumper on load, reset on unload

---
+++ R/onLoad.R
@@ -0,0 +1,15 @@
+
+prev <- NULL
+
+.onLoad <- function(libname, pkgname) {
+  prev <<- getOption("error")
+  options(error = dumper)
+}
+
+.onUnload <- function(libpath) {
+  options(error = prev)
+}                    
+
+dumper <- function() {
+  
+}
