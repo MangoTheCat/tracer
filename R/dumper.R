6c1edfc93d3dc82d873b94f2a4bc5ed3e8e2afd3
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-17 10:42:18 +0000

    Put dumper in separate file

---
+++ R/dumper.R
@@ -0,0 +1,37 @@
+
+data <- new.env(parent = emptyenv())
+
+dumper <- function() {
+  calls <- drop_last(sys.calls())
+  funcs <- lapply(seq_along(calls), sys.function)
+  
+  dump <- list(
+    nums  = format(seq_along(calls)),
+    calls = calls,
+    funcs = funcs,
+    envs  = vapply(funcs, function(x) environmentName(environment(x)), ""),
+    fnams = vapply(calls, function(x) as.character(x[[1]]), ""),
+    fargs = vapply(calls, get_call_args, ""),
+    files = lapply(funcs, getSrcFilename),
+    lines = lapply(funcs, getSrcLocation),
+    error = geterrmessage()
+  )
+
+  data$last_dump <- dump
+
+  invisible()
+}
+
+get_call_args <- function(call) {
+  ## No arguments
+  if (is.null(call[-1])) return("()")
+
+  ## Otherwise format them
+  call[[1]] <- as.symbol("foobar")
+  str <- format(call)
+  str[1] <- sub("^foobar", "", str[1])
+  if (length(str) > 1) {
+    str[-1] <- sub("^[ ]+", "", str[-1])
+  }
+  paste(str, collapse = "")
+}
